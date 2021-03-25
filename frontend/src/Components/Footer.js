44345ed6491002123fd48cfa9fb9ae9dcdb2de5b
Amanuel <schwartzfuss763@yahoo.com>
2021-03-25 11:22:09 -0500

    added backend folder with server, and footer navigation

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,10 +1,103 @@
 import React from 'react'
-import { Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col, Nav} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Footer = () => {
     return (
         
-        <Container>
+        <Container className="footer-container">
+            <Row>
+                <Col md={3}>
+                    <Nav className="footer-navigation">
+                        <h3>Navigation</h3>
+                        <LinkContainer to='/'>
+                            <Nav.Link>
+                                HOME
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/shop'>
+                            <Nav.Link>
+                                SHOP
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/contact'>
+                            <Nav.Link>
+                                CONTACT
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/about'>
+                            <Nav.Link>
+                                ABOUT
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Col>
+
+                <Col md={3}>
+                    <Nav className="footer-navigation">
+                        <h3>SOCIAL MEDIA</h3>
+
+                        <LinkContainer to='/'>
+                            <Nav.Link>
+                                TWITTER
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/shop'>
+                            <Nav.Link>
+                                INSTAGRAM
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/contact'>
+                            <Nav.Link>
+                                FACEBOOK
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/about'>
+                            <Nav.Link>
+                                YOUTUBE
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Col>
+
+                <Col md={3}>
+                    <Nav className="footer-navigation">
+                        <h3>Information</h3>
+
+                        <LinkContainer to='/'>
+                            <Nav.Link>
+                                Join our Team
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/shop'>
+                            <Nav.Link>
+                                Our Story
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/contact'>
+                            <Nav.Link>
+                                Sponsor
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/about'>
+                            <Nav.Link>
+                                Location
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Col>
+
+                <Col md={3}>
+                    <Nav className="footer-navigation">
+                        <LinkContainer to='/'>
+                            <Nav.Link className="footer-logo">
+                                NASHVILLE INTERIOR DESIGN
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Col>
+            </Row>
+        
             <Row>
                 <Col className='text-center py-3'>
                     Copyright &copy;
